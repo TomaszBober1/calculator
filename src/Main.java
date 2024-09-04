11dcaa1a553af26be9d8e90066be2a004dc4f957
TomaszBober1 <tbober.013@gmail.com>
2024-09-04 23:28:22 +0200

    commit changes to new branch

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.math.BigInteger;
+
+public class Main {
+
+    static BigInteger factorial(BigInteger number) {
+        if (!number.equals(BigInteger.valueOf(1)) && !number.equals(BigInteger.valueOf(0))) {
+            number = number.multiply(factorial(number.subtract(BigInteger.valueOf(1))));
+        } else if (number.equals(BigInteger.valueOf(0))) {
+            return BigInteger.valueOf(1);
+        } else {
+            return number;
+        }
+
+
+        return number;
+    }
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Dupa dupa!");
+        //double start = Time.start_counter();
+        //System.out.println(factorial(BigInteger.valueOf(100)));
+        //double time = Time.stop_counter(start) * Math.pow(10, -9);
+        //System.out.println(time + " seconds");
+
+        MainScreenGUI.guiCrate();
+        for (double i = 1.01; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
